486a333f4ffc24d11e060aeb1cef963b6bd7fb55
Dasom04 <slz_952@naver.com>
2024-06-07 13:00:27 +0900

    #3. 로그인, 회원가입 화면 구분, 유효성 검증

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Input from '../ui/Input';
 import Button from '../ui/Button';
 
-const AuthForm = () => {
+const AuthForm = ({ isLogin, onSubmit, credentialsInvalid }) => {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredName, setEnteredName] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
   const [enteredComfirmPassword, setEnteredComfirmPassword] = useState('');
+
+  const {
+    email: emailIsVaild,
+    name: nameIsVaild,
+    password: passwordIsVaild,
+    confirmPassoword: passwordDontMatch,
+  } = credentialsInvalid;
 
   const updateInputValueHandler = (inputType, enteredValue) => {
     console.log('inputType: ', inputType);
@@ -29,7 +36,16 @@ const AuthForm = () => {
     }
   };
 
-  const sumbmitHandler = () => {};
+  const sumbmitHandler = () => {
+    console.log('버튼 클릭됨! submit!');
+    // 사용자의 입력 상태값을 객체로 포장해서 AuthContent에게 넘긴다 -> 부모쪽에서 유효성 검증 할 것.
+    onSubmit({
+      email: enteredEmail,
+      name: enteredName,
+      password: enteredPassword,
+      confirmPassoword: enteredComfirmPassword,
+    });
+  };
 
   return (
     <View>
@@ -41,23 +57,40 @@ const AuthForm = () => {
           // bind에 제공되는 첫번째 인수는 곧 실행할 함수의  this 키워드로 설정됩니다.
           // 두 번째 인수는 지정한 함수에 전달할 값을 세팅하면 됩니다.
           onUpdateValue={updateInputValueHandler.bind(this, 'email')}
+          isInVaild={emailIsVaild}
+          value={enteredEmail}
         />
-        <Input
-          label='이름'
-          onUpdateValue={updateInputValueHandler.bind(this, 'name')}
-        />
+        {!isLogin && (
+          <Input
+            label='이름'
+            onUpdateValue={updateInputValueHandler.bind(this, 'name')}
+            isInVaild={nameIsVaild}
+            value={enteredName}
+          />
+        )}
         <Input
           label='비밀번호'
           secure
           onUpdateValue={updateInputValueHandler.bind(this, 'password')}
+          isInVaild={passwordIsVaild}
+          enteredPassword={enteredPassword}
         />
-        <Input
-          label='비밀번호 확인'
-          secure
-          onUpdateValue={updateInputValueHandler.bind(this, 'comfirmPassword')}
-        />
-        <View>
-          <Button onPress={sumbmitHandler}>회원가입</Button>
+        {!isLogin && (
+          <Input
+            label='비밀번호 확인'
+            secure
+            onUpdateValue={updateInputValueHandler.bind(
+              this,
+              'comfirmPassword',
+            )}
+            isInVaild={passwordDontMatch}
+            value={enteredComfirmPassword}
+          />
+        )}
+        <View style={styles.button}>
+          <Button onPress={sumbmitHandler}>
+            {isLogin ? '로그인' : '회원가입'}
+          </Button>
         </View>
       </View>
     </View>
@@ -65,3 +98,9 @@ const AuthForm = () => {
 };
 
 export default AuthForm;
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 12,
+  },
+});
--- components/ui/Input.js
+++ components/ui/Input.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const Input = ({ label, keyboardType, secure, onUpdateValue, value }) => {
+const Input = ({
+  label,
+  keyboardType,
+  secure,
+  onUpdateValue,
+  value,
+  isInvaild,
+}) => {
   return (
     <View style={styles.inputContainer}>
-      <Text>{label}</Text>
+      <Text style={isInvaild && styles.labelInvaild}>{label}</Text>
       <TextInput
-        style={styles.input}
+        style={[styles.input, isInvaild && styles.inputInValid]} // 스타일 적용이 두개 이상이면 배열로 묶어서 전달.
         autoCapitalize='none'
         keyboardType={keyboardType}
         secureTextEntry={secure}
@@ -24,11 +31,17 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginVertical: 8,
   },
+  labelInvaild: {
+    color: Colors.error500,
+  },
   input: {
     paddingVertical: 8,
     paddingHorizontal: 6,
     backgroundColor: Colors.primary100,
     borderRadius: 4,
     fontSize: 16,
+  },
+  inputInValid: {
+    backgroundColor: Colors.error100,
   },
 });
