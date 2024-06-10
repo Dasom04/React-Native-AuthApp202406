718263b5f6edae21f3950ccfc2bca2b5ae4e5782
Dasom04 <slz_952@naver.com>
2024-06-10 12:47:09 +0900

    오타 수정

--- util/auth.js
+++ util/auth.js
@@ -1,17 +1,17 @@
-const AUTH_URL = 'http://192.168.0.231:8181/api/ayth';
+const AUTH_URL = 'http://192.168.0.231:8181/api/auth';
 
 const authenticate = async (email, password) => {
   const url = AUTH_URL + '/signin';
   const res = await fetch(url, {
     method: 'POST',
-    headers: { 'content-type': 'application/json' },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       email,
       password,
     }),
   });
 
-  const result = await resizeBy.json();
+  const result = await res.json();
   console.log('로그인 결과: ', result);
 
   return result.token;
